51c308076808478395c902d4f95f603e6a100a55
lushiouscoat <fricodelka71@gmail.com>
2026-04-20 23:40:38 +0300

    fix home manager

--- flake.nix
+++ flake.nix
@@ -1,22 +1,23 @@
 {
-  description = "MY flake";
+  description = "My flake"; 
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, disko, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [ ./system/configuration.nix ];
     };
 
-    homeConfigurations.lushious.nixos = home-manager.lib.homeManagerConfiguration {
+    homeConfigurations."lushious@nixos" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       modules = [ ./home-manager/home.nix ];
     };
