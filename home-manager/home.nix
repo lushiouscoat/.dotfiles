fab4dd8000dcb1bbc2cb076b487d18aa7fd967e9
lushiouscoat <fricodelka71@gmail.com>
2026-04-21 08:30:18 +0300

    3 edit flake home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,12 +3,47 @@
   home.homeDirectory = "/home/lushious";
 
   home.packages = with pkgs; [
+    # command tools
+    fastfetch
+    htop
+
+    # archives
+    zip
+    unzip
+
+    # coding
     neovim
-    git
-    firefox
   ];
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "lushiouscoat";
+    userEmail = "fricodelka71@gmail.com";
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+	size = 12;
+	draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      sysrebuild = "sudo nixos-rebuild switch --flake ./#nixos";
+      homerebuild = "home-manager switch";
+    };
+  };
 
   home.stateVersion = "25.11";
 }
