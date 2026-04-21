d381f93d2d8611517e2ae66ac2ffa651a334fc89
lushiouscoat <fricodelka71@gmail.com>
2026-04-21 16:01:23 +0300

    im tired

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,26 +1,43 @@
 { config, pkgs, ... }: {
   home.username = "lushious";
   home.homeDirectory = "/home/lushious";
-
-  # Minimal packages — you can add the rest later
   home.packages = with pkgs; [
+    # command tools
     fastfetch
     htop
+    # archives
     zip
     unzip
+    # coding
     neovim
   ];
-
   programs.git = {
     enable = true;
     settings = {
       user = {
         name = "lushiouscoat";
         email = "fricodelka71@gmail.com";
+      };    };
+  };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+	size = 12;
+        draw_bold_text_with_bright_colors = true;
       };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
     };
   };
-
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {      sysrebuild = "sudo nixos-rebuild switch --flake ./#nixos";
+      homerebuild = "home-manager switch";
+    };
+  };
   programs.home-manager.enable = true;
   home.stateVersion = "24.11";
 }
