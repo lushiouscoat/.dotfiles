417b71c4cbb90e3bda4d2707018f3e4e9e4b7b61
lushiouscoat <fricodelka71@gmail.com>
2026-04-21 09:21:08 +0300

    Minimal home.nix — fixes evaluation error

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,49 +2,22 @@
   home.username = "lushious";
   home.homeDirectory = "/home/lushious";
 
+  # Minimal packages — you can add the rest later
   home.packages = with pkgs; [
-    # command tools
     fastfetch
     htop
-
-    # archives
     zip
     unzip
-
-    # coding
     neovim
   ];
-
 
   programs.git = {
     enable = true;
     settings = {
       user = {
-        name = "lushiouscoat";        
+        name = "lushiouscoat";
         email = "fricodelka71@gmail.com";
       };
-    };
-  };
-
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-	size = 12;
-	draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    shellAliases = {
-      sysrebuild = "sudo nixos-rebuild switch --flake ./#nixos";
-      homerebuild = "home-manager switch";
     };
   };
 
