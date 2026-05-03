17e79931c855152910cd1e67e73e806d1834a01f
lushiouscoat <fricodelka71@gmail.com>
2026-05-03 17:43:58 +0300

    afggh6

--- home-manager/programs/media.nix
+++ home-manager/programs/media.nix
@@ -7,8 +7,6 @@
   services.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
   };
